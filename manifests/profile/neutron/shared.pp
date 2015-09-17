6575cf9439e59bfc8a5a8572e3c36c9d5d8a58df
Zdenek Janda <zdenek.janda@cloudevelops.com>
2015-09-17 13:14:16 +0200

    enable compute classes

---
+++ manifests/profile/neutron/shared.pp
@@ -0,0 +1,26 @@
+class openstack_base::profile::neutron::shared {
+
+  class { '::neutron':
+    enabled               => true,
+    bind_host             => '0.0.0.0',
+    rabbit_host           => $openstack_base::rabbitmq_ip,
+    rabbit_user           => 'openstack',
+    rabbit_password       => $openstack_base::rabbitmq_password,
+    verbose               => true,
+    debug                 => false,
+    core_plugin           => 'ml2',
+    service_plugins       => ['router','firewall','lbaas','metering'],
+    allow_overlapping_ips => true,
+  }
+
+  class { '::neutron::plugins::ml2':
+    type_drivers         => ['vxlan', 'local', 'vlan', 'flat'],
+    tenant_network_types => ['vxlan'],
+    vxlan_group          => '239.1.1.1',
+    mechanism_drivers    => ['openvswitch'],
+    vni_ranges           => ['65537:69999'], #VXLAN
+    tunnel_id_ranges     => ['65537:69999'], #GRE
+    network_vlan_ranges  => ['vlannet:802:826'],
+  }
+
+}
