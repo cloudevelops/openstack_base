fb73b9fad279fa19c73a82b2b4ec2191d4bacb58
Zdenek Janda <zdenek.janda@cloudevelops.com>
2015-09-17 17:48:39 +0200

    fix keystonerc

--- manifests/environment/sourcefiles.pp
+++ manifests/environment/sourcefiles.pp
@@ -1,6 +1,6 @@
 class openstack_base::environment::sourcefiles {
 
-  file { '/root/.keystonerc':
+  file { '/root/keystonerc_admin':
     ensure  => present,
     content =>
       "export OS_AUTH_URL=http://${openstack_base::keystone_ip}:35357/v2.0
@@ -9,11 +9,12 @@ export OS_PASSWORD=${openstack_base::admin_password}
 export OS_TENANT_NAME=admin
 export OS_VOLUME_API_VERSION=2
 ",
+    mode => 400,
   }
 
   file_line { 'keystonerc':
     path => '/root/.bashrc',
-    line => 'source ~/.keystonerc',
+    line => 'source ~/keystonerc_admin',
   }
 
 }
