6b0430c689dec74f655854a848503baf0a090134
Zdenek Janda <zdenek.janda@cloudevelops.com>
2015-09-17 23:05:54 +0200

    add publiclb

--- manifests/profile/publiclb/base.pp
+++ manifests/profile/publiclb/base.pp
@@ -2,4 +2,21 @@ class openstack_base::profile::publiclb::base {
 
   include openstack_base
 
+  include nginx_base
+
+  nginx::resource::vhost { 'openstack':
+    ssl => true,
+    ssl_cert => "/var/lib/puppet/ssl/certs/${fqdn}.pem",
+    ssl_key => "/var/lib/puppet/ssl/private_keys/${fqdn}.pem",
+    server_name => [$openstack_base::horizon_fqdn],
+    vhost_cfg_append => $vhost_cfg_append,
+    proxy => "http://${openstack_base::horizon_ip}",
+    proxy_read_timeout => 1000,
+  }
+
+  nginx::resource::vhost { 'openstack-httpsredirect':
+    server_name => [$openstack_base::horizon_fqdn],
+    location_custom_cfg => { 'rewrite' => '^ https://$server_name$request_uri? permanent' },
+  }
+
 }
