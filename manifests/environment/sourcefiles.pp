588da3255cf76afc221f456059af784e0c145fb5
Zdenek Janda <zdenek.janda@cloudevelops.com>
2015-09-17 16:58:16 +0200

    fix sourcefiles

--- manifests/environment/sourcefiles.pp
+++ manifests/environment/sourcefiles.pp
@@ -3,9 +3,9 @@ class openstack_base::environment::sourcefiles {
   file { '/root/keystonerc_admin':
     ensure  => present,
     content =>
-      "export OS_AUTH_URL=http://${local_ip}:35357/v2.0
+      "export OS_AUTH_URL=http://${openstack_base::keystone_ip}:35357/v2.0
 export OS_USERNAME=admin
-export OS_PASSWORD=${admin_password}
+export OS_PASSWORD=${openstack_base::admin_password}
 export OS_TENANT_NAME=admin
 export OS_VOLUME_API_VERSION=2
 ",
