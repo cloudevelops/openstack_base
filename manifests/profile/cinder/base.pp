b9e99d997d89bb03391949cf85a7419897ac8e26
Zdenek Janda <zdenek.janda@cloudevelops.com>
2015-09-17 20:40:14 +0200

    add cinder basics

--- manifests/profile/cinder/base.pp
+++ manifests/profile/cinder/base.pp
@@ -1,5 +1,22 @@
 class openstack_base::profile::cinder::base {
 
   include openstack_base
+  include openstack_base::profile::cinder::shared
+
+  class { 'cinder::api':
+    enabled           => true,
+    keystone_user     => 'cinder',
+    keystone_password => $openstack_base::admin_password,
+    auth_uri          => "http://${openstack_base::keystone_ip}:5000/v2.0",
+    identity_uri      => "http://${openstack_base::keystone_ip}:35357",
+    sync_db           => true,
+    service_workers   => $api_workers,
+    validate          => true, # Fails with a V2 API endpoint
+  }
+
+  class { 'cinder::scheduler':
+    enabled          => true,
+    scheduler_driver => 'cinder.scheduler.simple.SimpleScheduler',
+  }
 
 }
---
+++ manifests/profile/cinder/shared.pp
@@ -0,0 +1,11 @@
+class openstack_base::profile::cinder::shared {
+
+  class { 'cinder':
+    database_connection => "mysql://cinder:${openstack_base::admin_password}@${openstack_base::mysql_ip}/cinder",
+    rabbit_userid       => 'openstack',
+    rabbit_password     => $openstack_base::rabbitmq_password,
+    rabbit_host         => $openstack_base::rabbitmq_ip,
+    verbose             => true,
+  }
+
+}
