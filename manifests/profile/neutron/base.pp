d5ab4f619f3819e7828c2b4a789b724007a5523d
Zdenek Janda <zdenek.janda@cloudevelops.com>
2015-09-17 17:40:57 +0200

    neutron server notify fixed

--- manifests/profile/neutron/base.pp
+++ manifests/profile/neutron/base.pp
@@ -12,12 +12,13 @@ class openstack_base::profile::neutron::base {
     database_connection              => "mysql://neutron:${openstack_base::neutron_mysql_password}@${openstack_base::mysql_ip}/neutron?charset=utf8",
     sync_db                          => true,
     allow_automatic_l3agent_failover => true,
-    nova_url                         => "http://${openstack_base::nova_ip}:8774/v2",
   }
 
   class { '::neutron::server::notifications':
     nova_admin_tenant_name => 'services',
     nova_admin_password    => $openstack_base::admin_password,
+    nova_url               => "http://${openstack_base::nova_ip}:8774/v2",
+    nova_admin_auth_url    => "http://${openstack_base::keystone_ip}:35357/v2.0"
   }
 
 }
