4b4123b7847e53bc6c323e66b1b0b1b446ed83f9
Zdenek Janda <zdenek.janda@cloudevelops.com>
2015-09-17 21:54:00 +0200

    fixed cinder pwd

--- manifests/profile/cinder/shared.pp
+++ manifests/profile/cinder/shared.pp
@@ -1,7 +1,7 @@
 class openstack_base::profile::cinder::shared {
 
   class { 'cinder':
-    database_connection => "mysql://cinder:${openstack_base::admin_password}@${openstack_base::mysql_ip}/cinder",
+    database_connection => "mysql://cinder:${openstack_base::cinder_mysql_password}@${openstack_base::mysql_ip}/cinder",
     rabbit_userid       => 'openstack',
     rabbit_password     => $openstack_base::rabbitmq_password,
     rabbit_host         => $openstack_base::rabbitmq_ip,
