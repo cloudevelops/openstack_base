a99aa0f174bc5ffb4be4360231532bd7e39c638d
Zdenek Janda <zdenek.janda@cloudevelops.com>
2015-09-17 19:54:02 +0200

    setup memcache

--- manifests/profile/memcache/base.pp
+++ manifests/profile/memcache/base.pp
@@ -2,4 +2,10 @@ class openstack_base::profile::memcache::base {
 
   include openstack_base
 
+  class {'memcached_base':
+    listen_ip => '0.0.0.0',
+    tcp_port  => '11211',
+    udp_port  => '11211',
+  }
+
 }
