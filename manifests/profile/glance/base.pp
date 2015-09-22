ed80dc1ebdafaa90e21accc14cff7c39f3d6ca22
Zdenek Janda <zdenek.janda@cloudevelops.com>
2015-09-22 13:00:10 +0200

    cirros img disabled

--- manifests/profile/glance/base.pp
+++ manifests/profile/glance/base.pp
@@ -29,26 +29,27 @@ class openstack_base::profile::glance::base {
     rabbit_use_ssl  => false,
   }
 
-  exec { 'retrieve_cirros_image':
-    command => 'wget -q http://download.cirros-cloud.net/0.3.4/\
-cirros-0.3.4-x86_64-disk.img -O /tmp/cirros-0.3.4-x86_64-disk.img',
-    unless  => [ "glance --os-username admin --os-tenant-name admin \
---os-password ${openstack_base::admin_password} --os-auth-url http://${openstack_base::keystone_ip}:35357/v2.0 \
-image-show cirros-0.3.4-x86_64" ],
-    path    => [ '/usr/bin/', '/bin' ],
-    require => [ Class['glance::api'], Class['glance::registry'] ]
-  }->
-  exec { 'add_cirros_image':
-    command => "glance --os-username admin --os-tenant-name admin --os-password \
-${openstack_base::admin_password} --os-auth-url http://${openstack_base::keystone_ip}:35357/v2.0 image-create \
---name cirros-0.3.4-x86_64 --file /tmp/cirros-0.3.4-x86_64-disk.img \
---disk-format qcow2 --container-format bare --is-public True",
-    # Avoid dependency warning
-    onlyif  => [ 'test -f /tmp/cirros-0.3.4-x86_64-disk.img' ],
-    path    => [ '/usr/bin/', '/bin' ],
-  }->
-  file { '/tmp/cirros-0.3.4-x86_64-disk.img':
-    ensure => absent,
-  }
-
+#
+#  exec { 'retrieve_cirros_image':
+#    command => 'wget -q http://download.cirros-cloud.net/0.3.4/\
+#cirros-0.3.4-x86_64-disk.img -O /tmp/cirros-0.3.4-x86_64-disk.img',
+#    unless  => [ "glance --os-username admin --os-tenant-name admin \
+#--os-password ${openstack_base::admin_password} --os-auth-url http://${openstack_base::keystone_ip}:35357/v2.0 \
+#image-show cirros-0.3.4-x86_64" ],
+#    path    => [ '/usr/bin/', '/bin' ],
+#    require => [ Class['glance::api'], Class['glance::registry'] ]
+#  }->
+#  exec { 'add_cirros_image':
+#    command => "glance --os-username admin --os-tenant-name admin --os-password \
+#${openstack_base::admin_password} --os-auth-url http://${openstack_base::keystone_ip}:35357/v2.0 image-create \
+#--name cirros-0.3.4-x86_64 --file /tmp/cirros-0.3.4-x86_64-disk.img \
+#--disk-format qcow2 --container-format bare --is-public True",
+#    # Avoid dependency warning
+#    onlyif  => [ 'test -f /tmp/cirros-0.3.4-x86_64-disk.img' ],
+#    path    => [ '/usr/bin/', '/bin' ],
+#  }->
+#  file { '/tmp/cirros-0.3.4-x86_64-disk.img':
+#    ensure => absent,
+#  }
+#
 }
