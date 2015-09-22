4a4a18fc9191e5f1960e33d35825f4b284f2a7c0
Zdenek Janda <zdenek.janda@cloudevelops.com>
2015-09-22 13:35:34 +0200

    added dnat for public endpoints

--- manifests/profile/publiclb/base.pp
+++ manifests/profile/publiclb/base.pp
@@ -19,4 +19,66 @@ class openstack_base::profile::publiclb::base {
     location_custom_cfg => { 'rewrite' => '^ https://$server_name$request_uri? permanent' },
   }
 
+  sysctl::value {
+    'net.ipv4.ip_forward':
+      value => '1';
+  }
+
+  firewall {
+    '101_dnat_for_keystone':
+      ensure => 'present',
+      table => 'nat',
+      chain => 'PREROUTING',
+      destination => $openstack_base::public_api_ip,
+      proto => 'tcp',
+      dport => '5000',
+      jump => 'DNAT',
+      todest => "${openstack_base::keystone_ip}:5000";
+    '101_dnat_for_glance':
+      ensure => 'present',
+      table => 'nat',
+      chain => 'PREROUTING',
+      destination => $openstack_base::public_api_ip,
+      proto => 'tcp',
+      dport => '9292',
+      jump => 'DNAT',
+      todest => "${openstack_base::glance_ip}:9292";
+    '101_dnat_for_nova':
+      ensure => 'present',
+      table => 'nat',
+      chain => 'PREROUTING',
+      destination => $openstack_base::public_api_ip,
+      proto => 'tcp',
+      dport => '8774',
+      jump => 'DNAT',
+      todest => "${openstack_base::nova_ip}:8774";
+    '101_dnat_for_nova_ec2':
+      ensure => 'present',
+      table => 'nat',
+      chain => 'PREROUTING',
+      destination => $openstack_base::public_api_ip,
+      proto => 'tcp',
+      dport => '8773',
+      jump => 'DNAT',
+      todest => "${openstack_base::nova_ip}:8773";
+    '101_dnat_for_neutron':
+      ensure => 'present',
+      table => 'nat',
+      chain => 'PREROUTING',
+      destination => $openstack_base::public_api_ip,
+      proto => 'tcp',
+      dport => '9696',
+      jump => 'DNAT',
+      todest => "${openstack_base::neutron_ip}:9696";
+    '101_dnat_for_cinder':
+      ensure => 'present',
+      table => 'nat',
+      chain => 'PREROUTING',
+      destination => $openstack_base::public_api_ip,
+      proto => 'tcp',
+      dport => '8776',
+      jump => 'DNAT',
+      todest => "${openstack_base::cinder_ip}:8776";
+  }
+
 }
