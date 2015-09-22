7a079322801c26aa678f0dd08c5b8599c463b12c
Zdenek Janda <zdenek.janda@cloudevelops.com>
2015-09-22 12:31:29 +0200

    sourcefiles deploy flags supported

--- manifests/environment/sourcefiles.pp
+++ manifests/environment/sourcefiles.pp
@@ -1,20 +1,25 @@
-class openstack_base::environment::sourcefiles {
+class openstack_base::environment::sourcefiles (
+  $deploy = true
+) {
 
-  file { '/root/keystonerc_admin':
-    ensure  => present,
-    content =>
-      "export OS_AUTH_URL=http://${openstack_base::keystone_ip}:35357/v2.0
+  if $deploy {
+    file { '/root/keystonerc_admin':
+      ensure  => present,
+      content =>
+        "export OS_AUTH_URL=http://${openstack_base::keystone_ip}:35357/v2.0
 export OS_USERNAME=admin
 export OS_PASSWORD=${openstack_base::admin_password}
 export OS_TENANT_NAME=admin
 export OS_VOLUME_API_VERSION=2
 ",
-    mode => 400,
-  }
+      mode => 400,
+    }
 
-  file_line { 'keystonerc':
-    path => '/root/.bashrc',
-    line => 'source ~/keystonerc_admin',
+    file_line { 'keystonerc':
+      path => '/root/.bashrc',
+      line => 'source ~/keystonerc_admin',
+    }
+
   }
 
 }
