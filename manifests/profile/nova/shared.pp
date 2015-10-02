644a24c75a584487386e2dece8cb811f33f0ddee
Zdenek Janda <zdenek.janda@cloudevelops.com>
2015-10-02 13:58:27 +0200

    added debug to nova

--- manifests/profile/nova/shared.pp
+++ manifests/profile/nova/shared.pp
@@ -1,4 +1,7 @@
-class openstack_base::profile::nova::shared {
+class openstack_base::profile::nova::shared (
+  $verbose = false,
+  $debug   = false,
+){
 
   class { 'nova':
     database_connection => "mysql://nova:${openstack_base::nova_mysql_password}@${openstack_base::mysql_ip}/nova?charset=utf8",
@@ -6,7 +9,8 @@ class openstack_base::profile::nova::shared {
     rabbit_password     => $openstack_base::rabbitmq_password,
     image_service       => 'nova.image.glance.GlanceImageService',
     glance_api_servers  => "${openstack_base::glance_ip}:9292",
-    verbose             => true,
+    verbose             => $verbose,
+    debug               => $debug,
     rabbit_host         => $openstack_base::rabbitmq_ip,
   }
 
