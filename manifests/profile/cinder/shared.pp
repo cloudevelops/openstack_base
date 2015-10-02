eb1bf1ffa0899b35b8622642bc67a76979c95c87
Zdenek Janda <zdenek.janda@cloudevelops.com>
2015-10-02 13:52:35 +0200

    enable debug log in cinder'

--- manifests/profile/cinder/shared.pp
+++ manifests/profile/cinder/shared.pp
@@ -1,11 +1,15 @@
-class openstack_base::profile::cinder::shared {
+class openstack_base::profile::cinder::shared (
+  $verbose = false,
+  $debug   = false,
+) {
 
   class { 'cinder':
     database_connection => "mysql://cinder:${openstack_base::cinder_mysql_password}@${openstack_base::mysql_ip}/cinder",
     rabbit_userid       => 'openstack',
     rabbit_password     => $openstack_base::rabbitmq_password,
     rabbit_host         => $openstack_base::rabbitmq_ip,
-    verbose             => true,
+    verbose             => $verbose,
+    debug               => $debug,
   }
 
   cinder_config {
