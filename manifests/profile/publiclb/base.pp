7759bf0f8dcda6d08f8322216007981b72fee13e
Zdenek Janda <zdenek.janda@cloudevelops.com>
2015-10-04 17:47:07 +0200

    fix snat

--- manifests/profile/publiclb/base.pp
+++ manifests/profile/publiclb/base.pp
@@ -126,20 +126,21 @@ class openstack_base::profile::publiclb::base {
 #      dport => '8773',
 #      jump => 'DNAT',
 #      todest => "${openstack_base::nova_ip}:8773";
-    '101_dnat_for_nova_novnc':
-      ensure => 'present',
-      table => 'nat',
-      chain => 'PREROUTING',
-      destination => $openstack_base::public_api_ip,
-      proto => 'tcp',
-      dport => '6080',
-      jump => 'DNAT',
-      todest => "${openstack_base::nova_ip}:6080";
+#    '101_dnat_for_nova_novnc':
+#      ensure => 'present',
+#      table => 'nat',
+#      chain => 'PREROUTING',
+#      destination => $openstack_base::public_api_ip,
+#      proto => 'tcp',
+#      dport => '6080',
+#      jump => 'DNAT',
+#      todest => "${openstack_base::nova_ip}:6080";
     '102_snat_for_management':
       ensure   => 'present',
       table    => 'nat',
       chain    => 'POSTROUTING',
       proto    => 'tcp',
+      destination => $openstack_base::public_api_ip,
       dport    => '6080',
       outiface => 'eth1',
       jump     => 'SNAT',
