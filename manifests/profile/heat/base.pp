f4e8e39d6b8e460492753b39f27e71aa535ded6e
Zdenek Janda <zdenek.janda@cloudevelops.com>
2015-10-12 21:10:57 +0200

    configure heat

--- manifests/profile/heat/base.pp
+++ manifests/profile/heat/base.pp
@@ -1,13 +1,14 @@
 class openstack_base::profile::heat::base {
 
   include openstack_base
+
   class { '::heat':
     keystone_password => $openstack_base::admin_password,
     database_connection => "mysql://heat:${openstack_base::heat_mysql_password}@${openstack_base::mysql_ip}/heat?charset=utf8",
   }
 
   class { '::heat::engine':
-    auth_encryption_key => 'whatever-key-you-like',
+    auth_encryption_key => $openstack_base::heat_auth_encryption_key,
   }
 
   class { '::heat::api': }
