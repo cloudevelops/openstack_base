87a796344380c6bedd8e7b6b412e249731dfef0e
Zdenek Janda <zdenek.janda@cloudevelops.com>
2015-10-12 21:55:27 +0200

    fix auth_uri

--- manifests/profile/heat/base.pp
+++ manifests/profile/heat/base.pp
@@ -7,6 +7,7 @@ class openstack_base::profile::heat::base {
     rabbit_password     => $openstack_base::rabbitmq_password,
     rabbit_host         => $openstack_base::rabbitmq_ip,
     identity_uri        => "http://${openstack_base::keystone_ip}:35357",
+    auth_uri            => "http://${openstack_base::keystone_ip}:5000/v2.0",
     keystone_ec2_uri    => "http://${openstack_base::keystone_ip}:5000/v2.0/ec2tokens",
     keystone_password   => $openstack_base::admin_password,
     database_connection => "mysql://heat:${openstack_base::heat_mysql_password}@${openstack_base::mysql_ip}/heat?charset=utf8",
