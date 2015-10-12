a186ee4d93caf43f42eb2aa44a5e808db8f89633
Zdenek Janda <zdenek.janda@cloudevelops.com>
2015-10-12 21:34:22 +0200

    fixed heat config

--- manifests/profile/heat/base.pp
+++ manifests/profile/heat/base.pp
@@ -3,7 +3,12 @@ class openstack_base::profile::heat::base {
   include openstack_base
 
   class { '::heat':
-    keystone_password => $openstack_base::admin_password,
+    rabbit_userid       => 'openstack',
+    rabbit_password     => $openstack_base::rabbitmq_password,
+    rabbit_host         => $openstack_base::rabbitmq_ip,
+    identity_uri        => "http://${openstack_base::keystone_ip}:35357",
+    keystone_ec2_uri    => "http://${openstack_base::keystone_ip}:5000/v2.0/ec2tokens",
+    keystone_password   => $openstack_base::admin_password,
     database_connection => "mysql://heat:${openstack_base::heat_mysql_password}@${openstack_base::mysql_ip}/heat?charset=utf8",
   }
 
